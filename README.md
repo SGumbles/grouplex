6068952f2d0f9d1aabbb23c0a6147114c888c32d
Sniffy Gumbles <sniffy@fastmail.com>
2024-02-23 16:18:37 -0800

    Holy lord it's been a while since I made an update. Ok this is the WIP.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# grouplex
-Discord bot for streaming movies from a Plex server.
+# Grouplex
+A Discord bot for streaming movies from a Plex server.
+
+## Installing
+Ok hotshot, decrypt that secrets file with
+
+`gpg -o secrets.json secrets.json.gpg`
+
+IF you're cool enough to know the secret password. OTHERWISE, you'll need to make your own from `secrets.json.template` and fill out the keys and tokens.
+
+### Getting your bots token
+This is all done in the Discord developers control panel where you will register the bot and generate an invite URL. More info here : https://discordpy.readthedocs.io/en/stable/discord.html
+
+The Plex token can be found following instructions here : https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+
+## Setting up the bot and running it
+Install all the dependencies with `pip install dependencies.txt` and then run it with `python bot/bot.py` and away you go. A great night of grouplex is right around the corner for you and your boys, no wives allowed.
+
+You will need to ensure that the dependencies for Chromium are installed.
+
+```console
+goodhusband@NoWivesPC:~$ playwright install chromium
+Browser Chromium installed...
+You are a good husband and it's perfectly normal what you do with your friends.
+goodhusband@NoWivesPC:~$
+```
+
+### Running inside WSL
+You will need to do some fancy pants detective work to get the host IP address. This also applies if you're running the bot in a VM or something similar. 
+
+From a WSL terminal do this
+```console
+goodhusband@NoWivesPC:~$ ip route list
+default via 172.30.192.1 dev eth0 proto kernel
+172.30.192.0/20 dev eth0 proto kernel scope link src 172.30.202.82
+```
+In this case, our external IP is `172.30.192.1` so our Plex host URL is `http://172.30.192.1:32400` or whatever port you have your Plex server bound to.
--- bot/bot.py
+++ bot/bot.py
@@ -1,73 +1,238 @@
-from typing import Type
-from discord.app_commands.tree import CommandTree
-from discord.ext.commands.bot import _default
+from typing import Type, List, Optional
 from plexapi.server import PlexServer
+from plexapi.media import Media
+import pathlib
 import json
 import discord
-from discord.ext import commands
+import re
+import argparse
+from discord import app_commands
+from discord.ui import Select
+from urllib.parse import parse_qs, urlsplit, urlunsplit
+
+import asyncio
+from playwright.async_api import async_playwright, Request, Playwright, Browser
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3.connectionpool").setLevel('INFO')
+logging.getLogger("asyncio").setLevel('INFO')
+logger = logging.getLogger("GrouPlexBot")
 
-intents = discord.Intents.default()
-discordClient = discord.Client(intents=intents)
+############################
+###
+### CONFIG
+###
+############################
+## Wait for this long to try and connect / dick around with Plex server connections
+PLEX_TIMEOUT = 4
 
+SECRETS = {}
+SECRETS_JSON_FILE_PATH = pathlib.Path(__file__).parent.joinpath('../secrets.json').resolve()
+FRIENDS = {}
+FRIENDS_JSON_FILE_PATH = pathlib.Path(__file__).parent.joinpath('../cached_friends.json').resolve()
 
-class GrouPlexBot(commands.Bot):
-	def __init__(self) -> None:
-		intents = discord.Intents.default()
-		super().__init__(command_prefix=commands.when_mentioned_or('$'),intents=intents)
+PLEX_SERVER_POOL = []
+PLEX_CLIENT = None
 
-		self.add_command(self.show_playlist)
-		self.add_command(self.play)
-		self.add_command(self.pause)
-		self.add_command(self.call_nigel_gay)
-
-	async def setup_hook(self):
-		the_guild_id = discord.Object(id=SECRETS['test_guild_id'])
-		# This copies the global commands over to your guild.
-		self.tree.copy_global_to(guild=the_guild_id)
-		await self.tree.sync()
-
-	@commands.command()
-	async def show_playlist(ctx,*args):
-		pass
-
-	@commands.command()
-	async def play(ctx,*args):
-		pass
-	@commands.command()
-	async def call_nigel_gay(ctx,*args):
-		pass
-	@commands.command()
-	async def pause(ctx,*args):
-		pass
-
-
-# @discordClient.event
-# async def on_ready():
-# 	print("Aight fam, we logged in yo.")
-
-# @discordClient.event
-# async def on_message(message):
-# 	if message.content.startswith("$hello"):
-# 		await message.channel.send("Haaaay!")
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description="Runs a Discord bot so you and your boys can do FUN stuff without the wife knowing. Fun, normal")
+	parser.add_argument("--force-friends-search", action="store_true")
+	args = parser.parse_args()
 
 try:
-	import pathlib
-	p = pathlib.Path(__file__).parent.joinpath('../secrets.json').resolve()
-	with open(p) as f:
+	with open(SECRETS_JSON_FILE_PATH) as f:
 		SECRETS = json.load(f)
 except:
-	print(
-		"""
-		You need to have a secrets.json file in the root directory (or decrypt the SUPER SECRET one already in the repo).
-		"""
-	)
+	logger.critical("You need to have a secrets.json file in the root directory (or decrypt the SUPER SECRET one already in the repo).")
 	exit()
-bot = GrouPlexBot()
-bot.run(SECRETS['discord_bot_key'])
 
-# Get the stupid IP from `ip route list` if on WSL
-# plex = PlexServer()
-# print(plex.clients())
+
+############################
+###
+### PLEX INTERFACE
+###
+############################
+
+def populate_friends():
+	FRIENDS = {}
+	async def pw_loop(pw: Playwright):
+		browser = await pw.webkit.launch(headless=False)
+		page = await browser.new_page()
+		
+		def pageCallback(req:Request):
+			args = parse_qs(req.url)
+			if 'X-Plex-Token' in args and re.search(r"(\d{1,3}-?){4}\.\w+\.plex\.direct:\d+",req.url):
+				## Ok save this bad boy
+				parts = urlsplit(req.url)
+				url = urlunsplit( (parts[0],parts[1],*['']*3) ) #Note: *['']*3 is one less key-stroke than '','','' THEREFORE it is better
+				token = args['X-Plex-Token'][0]
+				if token not in FRIENDS:
+					logger.info(f"Saving server token for {url} --- {token}")
+				FRIENDS[token] = url
+				
+		page.on('request',pageCallback)
+		browser_closed = False
+		def close_browser(b:Browser):
+			nonlocal browser_closed
+			browser_closed = True
+		browser.on("disconnected",close_browser)
+		page.on('close',close_browser)
+		await page.goto(SECRETS['plex_url'])
+		## Wait for the user to close the browser
+		while not browser_closed:
+			await asyncio.sleep(1)
+		await browser.close()
+	async def run_browser():
+		async with async_playwright() as pw:
+			await pw_loop(pw)
+	logger.info("*********************************************")
+	logger.info("*\tRunning browser for sign-in")
+	logger.info("*\tSign in to Plex, click around some libraries and then just close the window. Easy Peasy!")
+	logger.info("*********************************************")
+	asyncio.run(run_browser())
+	with open(FRIENDS_JSON_FILE_PATH,'w') as f:
+		json.dump(FRIENDS,f)
+
+if args.force_friends_search:
+	populate_friends()
+
+try:
+	with open(FRIENDS_JSON_FILE_PATH) as f:
+		FRIENDS = json.load(f)
+except:
+	logger.warning("Can't find cached_friends.json, forcing sign-in...")
+	populate_friends()
+	exit()
+
+logger.info("Creating Plex server pool")
+
+
+for token, url in FRIENDS.items():
+	logger.info(f"Connecting to {url} with token {token} and timeout {PLEX_TIMEOUT}")
+	try:
+		PLEX_SERVER_POOL.append(PlexServer(baseurl=url,token=token,timeout=PLEX_TIMEOUT))
+	except Exception as e:
+		logger.warning(f"Failed to connect to server : {e}")
+
+def print_clients_and_exit():
+	logger.fatal("Set 'plex_client_name' in secrets.json to one of the following:")
+	for server in PLEX_SERVER_POOL:
+		logger.fatal(f"From {server.friendlyName}:")
+		for i,c in enumerate(server.clients()):
+			logger.fatal(f"\t{i+1}. {c.title}")
+	exit()
+
+
+if not 'plex_client_name' in SECRETS:
+	print_clients_and_exit()
+	
+for server in PLEX_SERVER_POOL:
+	try:
+		PLEX_CLIENT = server.client(SECRETS['plex_client_name'])
+		if PLEX_CLIENT is not None:
+			break
+	except:
+		pass
+
+if PLEX_CLIENT is None:
+	logger.fatal(f"Can't find client with name {SECRETS['plex_client_name']}")
+	print_clients_and_exit()
+
+async def coro_mega_search(media_name:str):
+	search_threads = [ asyncio.to_thread(s.search,media_name) for s in PLEX_SERVER_POOL]
+	results =  await asyncio.gather(*search_threads)
+	# Flatten all the results
+	hub_types = ['movie','episode']
+	return [x for serv in results for x in serv if x.type in hub_types]
+	
+def mega_search(media_name:str):
+	try:
+		results = asyncio.run(coro_mega_search(media_name))
+	except:
+		logging.fatal("Search broked")
+	## Process the results? Sort by resolution?
+	return results
+
+
+
+############################
+###
+### DISCORD INTERFACE
+###
+############################
+
+
+# results = mega_search("Openheimer")
+# exit()
+
+MAIN_GUILD = discord.Object(id=SECRETS['main_guild_id'])
+class GroupPlex(discord.Client):
+	def __init__(self):
+		intents = discord.Intents.default()
+		super().__init__(intents=intents)
+		self.tree = app_commands.CommandTree(self)
+	async def setup_hook(self) -> None:
+		self.tree.copy_global_to(guild=MAIN_GUILD)
+		await self.tree.sync(guild=MAIN_GUILD)
+
+gp = GroupPlex()
+
+@gp.event
+async def on_ready():
+	print("------------------------------------------------")
+	print(f"Bot running as {gp.user} (ID: {gp.user.id})")
+	print("------------------------------------------------")
+
+async def media_autocomplete(
+	interaction: discord.Interaction,
+	current: str,
+) -> List[app_commands.Choice[str]]:
+	res = await coro_mega_search(current)
+	return [
+		app_commands.Choice(name=i.title, value=i.title)
+		for i in res
+	]
+
+def get_human_video_quality(media:Media):
+	if media.height < 720:
+		return "😒🥔"
+	elif media.height < 1080:
+		return "720🥀"
+	elif media.height < 2160:
+		return "1080🍌"
+	else:
+		return "4k🍆💦"
+
+
+@gp.tree.command()
+@app_commands.describe(media='Title of media')
+@app_commands.describe(play_now='Add to the queue or play right now')
+@app_commands.autocomplete(media=media_autocomplete)
+async def play(interaction: discord.Interaction, media:str, play_now:bool):
+	"""Play a movie or show, either adding it to the queue or playing it now"""
+	res = await coro_mega_search(media)
+	if len(res) > 1:
+		options = [discord.SelectOption(label=f"{m.title} ({get_human_video_quality(m.media[0])} res.) on {m._server.friendlyName}",
+					value=m.key,
+					description=m.summary) 
+			for m in res]
+		select_menu = discord.ui.Select(placeholder="Select which media to play",options=options)
+		await interaction.response.send_modal(select_menu)
+		print("Aight we gon dun play dis")
+		print(select_menu.values)
+
+	if play_now:
+		pass
+	# results_server = plex_server.search(search_string)
+	# results_library = plex_server.library.search(search_string)
+
+	# print(results_server)
+	# print(results_library)
+
+	# hub_types = ['movie','episode']
+	# results = list(filter(lambda x: x.type in hub_types, results))
+	
+	# await interaction.response.send_message(f"Hello captain faggot face, I mean {interaction.user.mention}")
+
+gp.run(SECRETS['discord_bot_key'])
